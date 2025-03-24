2e8acfc7049da05cc8d9d0a8ebd6ee4050278cb9
Cleyton Caminha <cleytoncaminha01@gmail.com>
2025-03-24 01:01:53 -0300

    add cors

--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,12 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe({ whitelist: true, forbidNonWhitelisted: true }));
 
+  app.enableCors({
+    origin: ['http://localhost:3000'],
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+  });
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
